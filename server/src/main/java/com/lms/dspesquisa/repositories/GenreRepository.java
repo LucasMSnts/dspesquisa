9b35ea4888a716e92502dc7462f35effc15f596f
Lucas Mauro <lucas_de_mauro@hotmail.com>
2020-09-17 23:51:43 -0300

    DTO, Service

--- server/src/main/java/com/lms/dspesquisa/repositories/GenreRepository.java
+++ server/src/main/java/com/lms/dspesquisa/repositories/GenreRepository.java
@@ -1,9 +1,11 @@
 package com.lms.dspesquisa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.lms.dspesquisa.entities.Genre;
 
+@Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 
 }
